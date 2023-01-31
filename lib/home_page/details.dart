34f4da67fa0e84300ca0a8eeb2e6768f0c96c543
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2023-01-31 20:43:23 +0700

    Update Ticket Page

--- lib/home_page/details.dart
+++ lib/home_page/details.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:fluttericon/typicons_icons.dart';
 
 class DetailsPage extends StatelessWidget {
   // const DetailsPage({super.key});
+
+  // var Ticket =
 
   @override
   Widget build(BuildContext context) {
@@ -21,20 +24,22 @@ class DetailsPage extends StatelessWidget {
           margin: EdgeInsets.only(top: 300),
           height: 1000,
           decoration: BoxDecoration(
-            color: Colors.white,
+            // color: Colors.white.withAlpha(220),
+            color: Color(0xffF7F7F7),
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(40),
               topRight: Radius.circular(40),
             ),
           ),
           child: ListView(
+            physics: BouncingScrollPhysics(),
             children: [
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 40),
-                height: 60,
+                height: 50,
                 decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(40),
                 ),
                 child: DefaultTabController(
                   length: 3,
@@ -114,16 +119,32 @@ class DetailsPage extends StatelessWidget {
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 30),
+                margin: EdgeInsets.symmetric(vertical: 25),
                 alignment: Alignment.center,
                 child: Text(
                   "2 tickets",
                   style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w600,
+                    fontWeight: FontWeight.w700,
                     fontSize: 30,
                   ),
                 ),
-              )
+              ),
+
+              // ticket
+              TicketBody(),
+              TicketMoney(),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              TicketBody(),
+              TicketMoney(),
+
+              SizedBox(
+                height: 30,
+              ),
+              // end ticket
             ],
           ),
         ),
@@ -142,6 +163,227 @@ class DetailsPage extends StatelessWidget {
           ),
           child: Icon(Icons.arrow_back_ios_outlined),
         ),
+      ),
+    );
+  }
+}
+
+class TicketMoney extends StatelessWidget {
+  // const TicketMoney({
+  //   Key? key,
+  // }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 50,
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      // height: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.airplane_ticket_rounded),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                "American Airline",
+                style: GoogleFonts.poppins(
+                  color: Colors.black.withOpacity(0.5),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "\$120",
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TicketBody extends StatelessWidget {
+  // const TicketBody({
+  //   Key? key,
+  // }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 50,
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      padding: EdgeInsets.symmetric(vertical: 20),
+      // height: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+          bottomLeft: Radius.circular(40),
+          bottomRight: Radius.circular(40),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            width: 400,
+            // height: 50,
+            // color: Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Icon(
+                        Icons.circle_outlined,
+                        size: 20,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Text(
+                      '-- -- -- -- -- --',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Icon(
+                        Icons.airplanemode_active,
+                        size: 20,
+                      ),
+                    ),
+                    Text(
+                      '-- -- -- -- -- --',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Icon(
+                        Icons.place_outlined,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'New York',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black.withOpacity(0.6),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25),
+                        child: Text(
+                          '12h',
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Great Britain',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black.withOpacity(0.6),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // verticalDirection: VerticalDirection.up,
+                  children: [
+                    Text(
+                      "10:40am",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "4.26.2021, Tue",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  // verticalDirection: VerticalDirection.up,
+                  children: [
+                    Text(
+                      "12:55am",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "4.27.2021, Wed",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
